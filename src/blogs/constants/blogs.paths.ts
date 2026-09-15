171583ba192e547951d8636b8481f7b841d69585
Tatyana Nemchina <23angorka07@gmail.com>
2026-09-15 10:54:25 +0300

    feat: added CRUD routes for blogs

---
+++ src/blogs/constants/blogs.paths.ts
@@ -0,0 +1,6 @@
+export const BLOGS_PATH = "/blogs";
+
+export const BLOGS_ROUTES = {
+  ROOT: "",
+  BY_ID: "/:blogId",
+} as const;
--- src/setup-app.ts
+++ src/setup-app.ts
@@ -1,10 +1,11 @@
 import express, { Express } from "express";
+import { blogsRouter } from "./blogs/routers/blogs.router";
+import { BLOGS_PATH } from "./blogs/constants/blogs.paths";
 
 export const setupApp = (app: Express) => {
   app.use(express.json());
 
-  app.get("/", (req, res) => {
-    res.status(200).send("Hello world!!!!!");
-  });
+  app.use(BLOGS_PATH, blogsRouter);
+
   return app;
 };
